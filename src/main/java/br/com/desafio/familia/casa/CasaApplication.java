7075dca9d4ad0df2d1859209209c5ca2ff89f8c0
Gabriel <gabrielneiva@hotmail.com>
2022-09-20 00:14:08 -0300

    Commit inicial

---
+++ src/main/java/br/com/desafio/familia/casa/CasaApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.familia.casa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafio/familia/casa/CasaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.familia.casa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
